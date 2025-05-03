80669657c5ef63273c46cf47a13b918ec66ce95b
sevchik-f8fe <kononovseva0627@gmail.com>
2025-05-03 15:27:41 +0300

    add quest schema

---
+++ schemas/Quest.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const QuestSchema = new mongoose.Schema({
+    startAt: { type: Date, default: Date.now, required: true },
+    winner: { type: mongoose.Schema.Types.ObjectId, ref: 'Team', default: null },
+});
+
+export default mongoose.model('Admin', AdminSchema)
